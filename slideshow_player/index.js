f5a29d6170eac12bc47916e23a3c2c574a9e2e62
Tran Ngoc Khai <khai.tran@ascendcorp.com>
2018-04-29 21:21:11 +0700

    Remove junk code

--- slideshow_player/index.js
+++ slideshow_player/index.js
@@ -16,26 +16,10 @@ fetchUrlBtn.onclick = function(){
     socket.emit("url", {url: urlTxt.value});
 }
 
-var slideshows = [];
-function addSlideshow(id, json, localize, needGlslTransitions) {
-    if (typeof json === "function") {
-        slideshows.push({
-            id: id,
-            enter: json
-        });
-    } else {
-        if (needGlslTransitions) json.transitions = GlslTransitions;
-        if (localize) Diaporama.localize(json, localize);
-        slideshows.push({
-            id: id,
-            enter: function (diaporama) {
-                diaporama.data = json;
-                return function () {};
-            }
-        });
-    }
-}
-addSlideshow("Ex.3", require("./example3/diaporama.json"), "./example3/", true);
+// Test
+var data = require("./example3/diaporama.json");
+data.transitions = GlslTransitions;
+Diaporama.localize(data, './example3/');
 
 // Socket.io command
 socket.on('connect', function(){
@@ -51,13 +35,14 @@ var diaporama = Diaporama(document.getElementById("diaporama"), null, {
     autoplay: false,
     loop: false
 });
+diaporama.data = data;
+
 diaporama.on("error", console.error.bind(console));
 diaporama.on("load", function () {
     console.log("Slideshow has loaded");
     if (canvas == null) {
         canvas = document.getElementById('diaporamaContainer').querySelector('canvas');
     }
-//        canvas = document.querySelector('canvas');
 });
 
 diaporama.on("ended", function () {
@@ -117,7 +102,6 @@ function renderVideo() {
                 var printInfos = text_utils.getLines(copied_context, subtitle, 1100);
                 resetIndex = printInfos.no_of_frames;
                 frameIndex = 0;
-                console.log(printInfos);
             }
         }
 
@@ -167,48 +151,3 @@ function resize() { // Responsive diaporama
 resize();
 
 window.diaporama = diaporama;
-
-// Synchronise the current slide
-
-var $currentSlide = document.getElementById("currentSlide");
-diaporama.on("slide", function (slide) {
-    $currentSlide.textContent = beautify(slide, null, 2, 76);
-    window.hljs.highlightBlock($currentSlide);
-});
-
-// Slideshows navs
-var $slideshows = document.getElementById("slideshows");
-var currentSlideshow = 0,
-    leave;
-slideshows.forEach(function (slideshow, i) {
-    var a = document.createElement("a");
-    $slideshows.appendChild(a);
-    a.target = "_self";
-    a.href = "#example/" + i;
-    a.textContent = slideshow.id;
-
-    diaporama.on("data", function () {
-        if (currentSlideshow === i) {
-            a.className = "active";
-        } else {
-            a.className = "";
-        }
-    });
-    if (i === currentSlideshow) {
-        leave = slideshow.enter(diaporama);
-    }
-});
-
-function syncHash() {
-    var maybeExample = (location.hash || "").match("example/([0-9]+)");
-    if (maybeExample) {
-        var i = parseInt(maybeExample[1]);
-        if (currentSlideshow === i) return;
-        leave();
-        currentSlideshow = i;
-        leave = slideshows[i].enter(diaporama);
-        diaporama.currentTime = 0;
-    }
-}
-window.addEventListener("hashchange", syncHash);
-syncHash();
