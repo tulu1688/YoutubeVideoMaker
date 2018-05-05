dfa6b92b3dc026fb929963f96ce838406eaf0fc8
Tran Ngoc Khai <khai.tran@ascendcorp.com>
2018-05-05 15:01:12 +0700

    SP05 - Create video detail view

--- video_generator/routers/view_router.js
+++ video_generator/routers/view_router.js
@@ -1,8 +1,29 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/videos', function(req, res, next) {
+var dal = require('../dal.js');
+
+router.get('/videos', function (req, res, next) {
     res.render('videos');
+});
+
+router.get('/videos/:videoId', function (req, res, next) {
+    dal.searchVideos({
+        id: req.params.videoId
+    }, function (err, data) {
+        if (err) {
+            res.statusCode = 500;
+            res.send('General error');
+        } else {
+            var video = data.shift()
+            if (video) {
+                res.render('video_detail', {video: video});
+            } else {
+                res.statusCode = 404;
+                res.send('Video with [' + req.params.videoId + '] id doesnot exist');
+            }
+        }
+    });
 });
 
 module.exports = router;
